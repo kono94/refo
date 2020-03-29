f4b50627d19c1d523276472282291416dd3261e9
Jan Löwenstrom <jan.loewenstrom@web.de>
2020-03-29 17:22:01 +0200

    add antGame analysis data and R Scripts and images

---
+++ OptimalityDifferentDiscountFactors.R
@@ -0,0 +1,37 @@
+# Libraries
+library(ggplot2)
+library(matrixStats)
+ta  <- as.matrix(read.table(file.choose(), sep=",", header = FALSE, skip = 1))
+ta <- t(ta)
+dim(ta)
+head(ta)
+
+# Create dummy data
+data <- data.frame(
+  y=ta[,1],
+  y2=ta[,2],
+  y3=ta[,3],
+  y4=ta[,4],
+  y5=ta[,5],
+  y6=ta[,6],
+  y7=ta[,7],
+  y8=ta[,8],
+  x=seq(1, length(ta[,1]))
+)
+ggplot(data, aes(x*1000)) +
+  labs(title="Durchschnittliche Zeitstempel pro gesammeltes Futter (Lernrate = 0.9)",
+       x ="Gesamtanzahl gesammeltes Futter", y = "Ø Zeitstempel pro Futter (log)", color = "Diskontierungsfaktor") +
+  scale_y_log10()+
+  geom_hline(yintercept=23, linetype="dashed")+
+  geom_text(aes(0,23,label = "opt", vjust = -1)) +
+  geom_line(aes(y = y, colour = "0.99"), size=1)+
+  geom_line(aes(y = y2, colour = "0.95"), size=1) +
+  geom_line(aes(y = y3, colour = "0.9"), size=1) +
+  geom_line(aes(y = y4, colour = "0.7"), size=1) +
+  geom_line(aes(y = y5, colour = "0.5"), size=1)+
+  geom_line(aes(y = y6, colour = "0.3"), size=1) +
+  geom_line(aes(y = y7, colour = "0.1"), size=1) +
+  geom_line(aes(y = y8, colour = "0.05"), size=1) 
+
+  
+  
--- src/main/java/example/ContinuousAnt.java
+++ src/main/java/example/ContinuousAnt.java
@@ -12,8 +12,10 @@ import java.io.File;
 import java.io.IOException;
 
 public class ContinuousAnt {
-    public static final String FILE_NAME = "converge05.txt";
+    public static final String FILE_NAME = "converge22.txt";
     public static void main(String[] args) {
+        int i = 4+4+4+6+6+6+8+10+12+14+14+16+16+16+18+18+18+20+20+20+22+22+22+24+24+24+24+26+26+26+26+26+28+28+28+28+28+30+30+30+30+32+32+32+34+34+34+36+36+38+40+42;
+        System.out.println(i/52f);
         File file = new File(FILE_NAME);
         try {
             file.createNewFile();
@@ -27,9 +29,12 @@ public class ContinuousAnt {
                 AntAction.values());
         rl.setDelay(0);
         rl.setNrOfEpisodes(1);
-        rl.setDiscountFactor(0.7f);
-        rl.setLearningRate(0.2f);
-        rl.setEpsilon(0.5f);
+        //0.99 0.9 0.5
+        //0.99 0.95 0.9 0.7 0.5 0.3 0.1
+        rl.setDiscountFactor(0.05f);
+        // 0.1, 0.3, 0.5, 0.7 0.9
+        rl.setLearningRate(0.9f);
+        rl.setEpsilon(0.2f);
         rl.start();
     }
 }
